9452f1a742825d94f5809fd822235d07599c48fc
Francisco Wherther <wsiriano83@gmail.com>
2026-01-05 23:10:36 -0300

    feat: add while sctructure package and exercise 01.

---
+++ learning-java-fundamentals/src/section01/repetition_sctructure_while/PasswordValidator.java
@@ -0,0 +1,25 @@
+package section01.repetition_sctructure_while;
+
+import java.util.Scanner;
+
+public class PasswordValidator {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite a senha: ");
+		int password = sc.nextInt();
+		
+		// The white loops continues as long as the password in not 2002
+		while (password != 2002) {
+			System.out.println("Senha inválida");
+			password = sc.nextInt();
+		}
+		
+		System.out.println("Acesso permitido");
+		
+		sc.close();
+
+	}
+
+}
