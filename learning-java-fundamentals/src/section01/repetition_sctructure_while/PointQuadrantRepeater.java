a1f168ea8dfe6dc4ac2fa27e1dbe5d06ed32e705
Francisco Wherther <wsiriano83@gmail.com>
2026-01-05 23:23:31 -0300

    feat: add exercise 02 (Point Quadrant Repeater)

---
+++ learning-java-fundamentals/src/section01/repetition_sctructure_while/PointQuadrantRepeater.java
@@ -0,0 +1,32 @@
+package section01.repetition_sctructure_while;
+
+import java.util.Scanner;
+
+public class PointQuadrantRepeater {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite as coordenadas X e Y: ");
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		// The loop continues as long as BOTH x and y are different from zero
+		while (x != 0 && y != 0) {
+			if (x > 0 && y > 0) {
+				System.out.println("primeiro");
+			} else if (x < 0 && y > 0) {
+				System.out.println("segundo");
+			} else if (x < 0 && y < 0) {
+				System.out.println("terceiro");
+			} else {
+				System.out.println("quarto");
+			}
+			
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}
+		
+		sc.close();
+	}
+}
