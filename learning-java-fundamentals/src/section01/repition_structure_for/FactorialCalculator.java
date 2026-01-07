15c427ec43e65b5bccd7b01f86ff18259a0054d4
Francisco Wherther <wsiriano83@gmail.com>
2026-01-07 14:50:18 -0300

    feat: add exercise 05 (Factorial Calculator)

---
+++ learning-java-fundamentals/src/section01/repition_structure_for/FactorialCalculator.java
@@ -0,0 +1,27 @@
+package section01.repition_structure_for;
+
+import java.util.Scanner;
+
+public class FactorialCalculator {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		
+		System.out.println("Digite um número para calcular o fatorial: ");
+		int n = sc.nextInt();
+		
+		// We start with 1 because it's the neutral element of multiplication 
+		int fact = 1;
+		
+		// The loop calculates: 1 * 2 * 3 * ... * N
+		for (int i = 1; i <= n; i++) {
+			fact = fact * i;
+		}
+		
+		System.out.println(fact);
+		
+		sc.close();
+
+	}
+
+}
