e420781d4f9f1e9cf9f791a3b52c367e86288c01
Francisco Wherther <wsiriano83@gmail.com>
2026-01-14 16:07:21 -0300

    feat: add Negatives exercises

---
+++ learning-java-fundamentals/src/section02/application/NegativesProgram.java
@@ -0,0 +1,34 @@
+package section02.application;
+
+import java.util.Scanner;
+
+public class NegativesProgram {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// Input: Number of elements (N)
+		System.out.println("Quantos números você irá digitar? ");
+		int n = sc.nextInt();
+		
+		// Vector declaration 
+		int[] numbers = new int[n];
+		
+		// Loop to read and store numbers in the vector
+		for (int i = 0; i < n; i++) {
+			System.out.println("Digite um número: ");
+			numbers[i] = sc.nextInt();
+		}
+		
+		// Output: Filtering and displaying negative numbers
+		System.out.println("Numero negativos: ");
+		for (int i = 0; i < n; i++) {
+			if (numbers[i] < 0) {
+				System.out.println(numbers[i]);
+			}
+		}
+		
+		sc.close();
+	}
+
+}
