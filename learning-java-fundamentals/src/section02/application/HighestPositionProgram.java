52aca6c077db1a9b1d26f73238f51cdb09827c52
Francisco Wherther <wsiriano83@gmail.com>
2026-01-15 14:10:57 -0300

    feat: add Highest Position exercise

---
+++ learning-java-fundamentals/src/section02/application/HighestPositionProgram.java
@@ -0,0 +1,39 @@
+package section02.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class HighestPositionProgram {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos números você irá digitar? ");
+		int n = sc.nextInt();
+		
+		// Vector of doubles
+		double[] numbers = new double[n];
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print("Digite um número: ");
+			numbers[i] = sc.nextDouble();
+		}
+		
+		// Logic to find the highest value and its position
+		double highestValue = numbers[0];
+		int position = 0;
+		
+		for (int i = 1; i < n; i++) {
+			if (numbers[i] > highestValue) {
+				highestValue = numbers[i];
+				position = i;
+			}
+		}
+		
+		System.out.println();
+		System.out.printf("Maior valor = %.1f%n", highestValue);
+		System.out.print("Posição do maior valor = " + position);
+	}
+
+}
