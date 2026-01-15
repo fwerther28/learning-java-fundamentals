0f9573e9a28a857bb1e8bae0cfc66f325c7a5069
Francisco Wherther <wsiriano83@gmail.com>
2026-01-15 14:31:25 -0300

    feat: add Vector Sum exercise (A + B = C)

---
+++ learning-java-fundamentals/src/section02/application/VectorSumProgram.java
@@ -0,0 +1,46 @@
+package section02.application;
+
+import java.util.Scanner;
+
+public class VectorSumProgram {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// Input: Vector size
+		System.out.print("Quantos valores terá cada vetor? ");
+		int n = sc.nextInt();
+
+		// Vectors declaration
+		int[] vectorA = new int[n];
+		int[] vectorB = new int[n];
+		int[] vectorC = new int[n];
+
+		// Reading values for Vector A
+		System.out.println("Digite os valores do vetor A: ");
+		for (int i = 0; i < n; i++) {
+			vectorA[i] = sc.nextInt();
+		}
+
+		// Reading values for Vector B
+		System.out.println("Digite os valores do vetor B: ");
+		for (int i = 0; i < n; i++) {
+			vectorB[i] = sc.nextInt();
+		}
+
+		// Generating Vector C by summing A and B
+		for (int i = 0; i < n; i++) {
+			vectorC[i] = vectorA[i] + vectorB[i];
+		}
+
+		// Output: Resulting Vector C
+		System.out.println();
+		System.out.println("Vetor Resultante: ");
+		for (int i = 0; i < n; i++) {
+			System.out.println(vectorC[i]);
+		}
+
+		sc.close();
+	}
+
+}
