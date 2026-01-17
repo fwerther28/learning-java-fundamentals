1691a17c62076b6629999fd7262594810e16d003
Francisco Wherther <wsiriano83@gmail.com>
2026-01-17 17:46:17 -0300

    feat: add Approved students exercise using parallel vectors

---
+++ learning-java-fundamentals/src/section02/application/ApprovedProgram.java
@@ -0,0 +1,41 @@
+package section02.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ApprovedProgram {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Quantos alunos serão digitados? ");
+		int n = sc.nextInt();
+
+		// Parallel vectors for names and grades
+		String[] names = new String[n];
+		double[] firstGrades = new double[n];
+		double[] secondGrades = new double[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Digite o nome, primeira e segunda nota do " + (i + 1) + "o aluno:");
+			sc.nextLine(); // Buffer cleaning
+			names[i] = sc.nextLine();
+			firstGrades[i] = sc.nextDouble();
+			secondGrades[i] = sc.nextDouble();
+		}
+
+		System.out.println("Alunos aprovados:");
+
+		// Logic to calculate average and check approval
+		for (int i = 0; i < n; i++) {
+			double average = (firstGrades[i] + secondGrades[i]) / 2.0;
+
+			if (average >= 6.0) {
+				System.out.println(names[i]);
+			}
+		}
+		
+		sc.close();
+	}
+}
