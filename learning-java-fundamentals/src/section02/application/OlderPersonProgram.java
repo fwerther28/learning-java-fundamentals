c8b54ef609dbd23191351a0a76061f42d68f56c9
Francisco Wherther <wsiriano83@gmail.com>
2026-01-17 17:29:33 -0300

    feat: add Older Person exercise using parallel vectors

---
+++ learning-java-fundamentals/src/section02/application/OlderPersonProgram.java
@@ -0,0 +1,43 @@
+package section02.application;
+
+import java.util.Scanner;
+
+public class OlderPersonProgram {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Quantas pessoas você irá digitar? ");
+		int n = sc.nextInt();
+
+		// Parallel vectors
+		String[] names = new String[n];
+		int[] ages = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Dados da " + (i + 1) + "a pessoa:");
+			System.out.print("Nome: ");
+			sc.nextLine(); // Buffer cleaning
+			names[i] = sc.nextLine();
+			System.out.print("Idade: ");
+			ages[i] = sc.nextInt();
+		}
+
+		// Logic to find the index of the oldest person
+		int maxAge = ages[0];
+		int oldestPosition = 0;
+
+		for (int i = 1; i < n; i++) {
+			if (ages[i] > maxAge) {
+				maxAge = ages[i];
+				oldestPosition = i;
+			}
+		}
+
+		System.out.println("Pessoa mais velha: " + names[oldestPosition]);
+
+		sc.close();
+	}
+
+}
