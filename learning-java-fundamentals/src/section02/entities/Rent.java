76b0a192d88447a0004dcb3d31462dd6a434f603
Francisco Wherther <wsiriano83@gmail.com>
2026-01-19 19:13:03 -0300

    feat: add Boarding House challenge (Pensionato)

---
+++ learning-java-fundamentals/src/section02/entities/Rent.java
@@ -0,0 +1,28 @@
+package section02.entities;
+
+public class Rent {
+	
+	private String name;
+	private String email;
+	
+	public Rent(String name, String email) {
+		this.name = name;
+		this.email = email;
+		
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	@Override
+	public String toString() {
+		return name + ", " + email;
+	}
+	
+}
