63415776840d8d19bc6fb9b638417c417ef92b98
Francisco Wherther <wsiriano83@gmail.com>
2026-03-10 20:44:07 -0300

    feat: implement clean JDBC insertion for Departments
    - Renamed program to JDBCInsertApplication for better clarity
    - Implemented multiple record insertion (D1, D2)
    - Removed commented code to maintain clean code standards
    - Added retrieval of auto-generated IDs

--- learning-java-fundamentals/src/theory_concepts/jdbc/application/insert/JDBCInsertApplication.java
+++ learning-java-fundamentals/src/theory_concepts/jdbc/application/insert/JDBCInsertApplication.java
@@ -10,7 +10,7 @@ import java.text.SimpleDateFormat;
 
 import theory_concepts.jdbc.db.DB;
 
-public class Program {
+public class JDBCInsertApplication {
 
 	public static void main(String[] args) {
 		
@@ -19,20 +19,6 @@ public class Program {
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-			/*
-			st = conn.prepareStatement(
-					"INSERT INTO seller"
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
-			
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);
-			*/
 			
 			st = conn.prepareStatement(
 					"insert into department (Name) values ('D1'),('D2')",
