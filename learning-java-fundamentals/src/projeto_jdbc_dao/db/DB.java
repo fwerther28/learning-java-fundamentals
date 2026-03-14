b813d8a3856da068fdb15e37806a8135f6687cae
Francisco Wherther <wsiriano83@gmail.com>
2026-03-14 17:39:19 -0300

    refactor: fix DbException import to use the correct project package

--- learning-java-fundamentals/src/projeto_jdbc_dao/db/DB.java
+++ learning-java-fundamentals/src/projeto_jdbc_dao/db/DB.java
@@ -9,8 +9,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
-import theory_concepts.jdbc.db.DbException;
-
 public class DB {
 	
 private static Connection conn = null;
