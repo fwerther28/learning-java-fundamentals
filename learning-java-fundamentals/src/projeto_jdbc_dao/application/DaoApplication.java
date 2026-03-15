ae11954944624be4d32bfd68eca6ef11f952f3be
Francisco Wherther <wsiriano83@gmail.com>
2026-03-15 19:42:14 -0300

    feat: implement Seller entity with Department association

--- learning-java-fundamentals/src/projeto_jdbc_dao/application/DaoApplication.java
+++ learning-java-fundamentals/src/projeto_jdbc_dao/application/DaoApplication.java
@@ -1,13 +1,19 @@
 package projeto_jdbc_dao.application;
 
+import java.util.Date;
+
 import projeto_jdbc_dao.model.entities.Department;
+import projeto_jdbc_dao.model.entities.Seller;
 
 public class DaoApplication {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
