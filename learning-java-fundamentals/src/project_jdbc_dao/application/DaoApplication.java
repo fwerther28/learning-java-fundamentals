7c9d42ea76e4eabf86104f6b0c5dcd650ded8abf
Francisco Wherther <wsiriano83@gmail.com>
2026-03-19 14:08:55 -0300

    feat: implement insert in SellerDaoJDBC with generated keys recovery

--- learning-java-fundamentals/src/project_jdbc_dao/application/DaoApplication.java
+++ learning-java-fundamentals/src/project_jdbc_dao/application/DaoApplication.java
@@ -1,6 +1,7 @@
 	package project_jdbc_dao.application;
 	
-	import java.util.List;
+	import java.util.Date;
+import java.util.List;
 
 import project_jdbc_dao.model.dao.DaoFactory;
 import project_jdbc_dao.model.dao.SellerDao;
@@ -29,5 +30,10 @@ import project_jdbc_dao.model.entities.Seller;
 			for (Seller obj : list) {
 				System.out.println(obj);
 			}
+			
+			System.out.println("\n=== TEST 4: seller insert ===");
+			Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+			sellerDao.insert(newSeller);
+			System.out.println("Inserted! New id = " +  newSeller.getId());
 		}
 	}
